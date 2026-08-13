f04530be1d2b24768b947faf0a877c0645ad92f3
Carlos <cherediar123@gmail.com>
2026-08-13 17:17:19 -0500

    initial commit

---
+++ src/main/java/com/cinetest/RetoCpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoCpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinetest/RetoCpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
